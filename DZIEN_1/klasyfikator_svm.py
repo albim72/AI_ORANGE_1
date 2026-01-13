17ba8f7d5f91ec8ce990663cf1fab65c4bb7edb4
albim72 <marcin.albiniak72@gmail.com>
2026-01-13 13:55:48 +0100

    Update klasyfikator_svm.py

--- DZIEN_1/klasyfikator_svm.py
+++ DZIEN_1/klasyfikator_svm.py
@@ -44,3 +44,48 @@ print(f"Zapisano obraz ze wzorem do pliku: {output_path}")
 
 #ładowanie danych ze zbioru digits
 digits = datasets.load_digits()
+
+#wizualizacja 4 pierwszych obrazów...
+_, axes = plt.subplots(nrows=1,ncols=4,figsize=(10,3))
+for ax,image,label in zip(axes,digits.images,digits.target):
+    ax.set_axis_off()
+    ax.imshow(image,cmap=plt.cm.gray_r,interpolation='nearest')
+    ax.set_title(f'Cyfra: {label}')
+plt.show()
+
+#liczba próbek
+n_samples = len(digits.images)
+print(n_samples)
+
+#przeksztsałcenie macierzy obrazu w wektor 8x8 = 64
+data = digits.images.reshape((n_samples,-1))
+
+#budowa klasyfikatora
+clf = svm.SVC(gamma=0.001, C=100.)
+X_trian,X_test,y_trian,y_test = train_test_split(data,digits.target,test_size=0.5,random_state=0,
+                                                 shuffle = False)
+#trening modelu
+clf.fit(X_trian,y_trian)
+
+predicted = clf.predict(X_test)
+
+    #wizualizacja 4 pierwszych obrazów...
+_, axes = plt.subplots(nrows=1,ncols=4,figsize=(10,3))
+for ax,image,prediction in zip(axes,X_test,predicted):
+    ax.set_axis_off()
+    image = image.reshape(8,8)
+    ax.imshow(image,cmap=plt.cm.gray_r,interpolation='nearest')
+    ax.set_title(f'Znaleziono: {prediction}')
+plt.show()
+
+#ocena modelu
+
+print(f"Rport klasyfikacyjny dla kalsyfikatora clf: {clf}"
+      f"\n{metrics.classification_report(y_test,predicted)}\n")
+
+Printer = metrics.ConfusionMatrixDisplay.from_predictions(y_test,predicted)
+
+#macierz pomyłek
+disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test,predicted)
+disp.plot()
+plt.show()
